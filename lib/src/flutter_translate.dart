cd76aa9d294e17d54cbb32ec0c0e2fa5019eed2e
Alexander von Bremen-Kühne <a.vonbremen-kuehne@neusta.de>
2020-07-16 23:45:39 +0200

    Added legacy options

--- lib/src/flutter_translate.dart
+++ lib/src/flutter_translate.dart
@@ -1,13 +1,27 @@
+import 'case_style.dart';
+
 enum ErrorLevel { ignore, warning, error }
 
 class FlutterTranslate {
   final String path;
   final ErrorLevel missingTranslations;
   final ErrorLevel missingArguments;
+  final LegacyOptions legacyOptions;
 
-  const FlutterTranslate(
-      {this.path,
-      this.missingTranslations = ErrorLevel.error,
-      this.missingArguments = ErrorLevel.error})
-      : assert(path != null);
+  const FlutterTranslate({
+    this.path,
+    this.missingTranslations = ErrorLevel.error,
+    this.missingArguments = ErrorLevel.error,
+    this.legacyOptions,
+  }) : assert(path != null);
+}
+
+class LegacyOptions {
+  final String separator;
+  final CaseStyle caseStyle;
+
+  const LegacyOptions({
+    this.caseStyle = CaseStyle.titleCase,
+    this.separator = "_",
+  });
 }
